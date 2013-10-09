c10197d8d66ef6aea73803c1f6cca974f136974d
Mathieu Keith <mathieu@mgkgroup.com>
2013-10-08 23:28:05 -0400

    finished static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  @base_title = 'Ruby on Rails Tutorial Sample App'
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it 'should have the right title' do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{@base_title} | Home")
+    end
+  end
+
+  describe 'Help page' do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it 'should have the right title' do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{@base_title} | Help")
+    end
+  end
+
+  describe 'About page' do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+
+    it 'should have the right title' do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{@base_title} | About")
+    end
+  end
+end
